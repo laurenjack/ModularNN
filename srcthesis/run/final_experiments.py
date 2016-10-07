3467386f68850ac63376282085b5b788dc600b57
Jack Laurenson <jack@Jacks-MacBook-Air.local>
2016-10-07 16:01:46 +1300

    Minor adjustments

--- srcthesis/run/final_experiments.py
+++ srcthesis/run/final_experiments.py
@@ -7,10 +7,10 @@ training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 runner = nr.NetworkRunner()
 
 #Setup the network
-sig_eta = 3.0
+sig_eta = 1.0
 sizes = [784, 30, 30, 10]
-acts = ['sig', 'or', 'sm']
-etas = [sig_eta, (0.1, 1.0), sig_eta]
+acts = ['sig', 'sig', 'sm']
+etas = [sig_eta, sig_eta, sig_eta]
 network = nf.mix_network(sizes, acts, etas)
 
 #run the experiment
--- srcthesis/run/simple_act_drawer.py
+++ srcthesis/run/simple_act_drawer.py
@@ -3,6 +3,14 @@ import numpy as np
 
 plt.rcParams["font.family"] = "cmr"
 
+def __draw_act(title, f):
+    x = np.arange(0, 5, 0.01)
+    y = f(x)
+    plt.figure(facecolor='white')
+    plt.title(title)
+    plt.plot(x, y)
+    plt.show()
+
 def draw_or():
     x = np.arange(0, 5, 0.01)
     y = 1.0 - np.exp(-x)
@@ -11,4 +19,8 @@ def draw_or():
     plt.plot(x, y)
     plt.show()
 
-draw_or()
+def draw_and():
+    f = lambda x : np.exp(-x)
+    __draw_act('Continuous And Activation Function', f)
+
+draw_and()
