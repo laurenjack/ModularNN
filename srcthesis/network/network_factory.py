5cf9baf13bc71727cda53e1662917531b759554e
Jack Laurenson <jack@Jacks-MacBook-Air.local>
2016-10-09 17:40:56 +1300

    Grr

--- srcthesis/network/network_factory.py
+++ srcthesis/network/network_factory.py
@@ -85,27 +85,34 @@ def __construct_w_b(act_string, s0, s1, hyp_params):
 
 
 def __construct_act(act_string, hp, reg):
-    # #Construct optimizers
-    # if reg:
-    #     sgd = Sgd_Regularisation(hp)
-    #     pos_sgd =
-    # else:
-    #     sgd = Sgd(hp)
-    #     pos_sgd = KeepPositiveSgd(eta)
 
     if act_string == 'sig':
-        sgd = Sgd(hp)
+        sgd = None
+        if reg:
+            sgd = Sgd_Regularisation(hp)
+        else:
+            sgd = Sgd(hp)
         return Sigmoid(sgd)
     if act_string == 'or':
         eta = hp[0]
-        pos_sgd = KeepPositiveSgd(eta)
+        if reg:
+            pos_sgd = KeepPositiveSgd(eta)
+        else:
+            pos_sgd = KeepPositiveRegSgd(eta)
         return NoisyOr(pos_sgd)
     if act_string == 'and':
         eta = hp[0]
-        pos_sgd = KeepPositiveSgd(eta)
+        if reg:
+            pos_sgd = KeepPositiveSgd(eta)
+        else:
+            pos_sgd = KeepPositiveRegSgd(eta)
         return NoisyAnd(pos_sgd)
     if act_string == 'sm':
-        sgd = Sgd(hp)
+        sgd = None
+        if reg:
+            sgd = Sgd_Regularisation(hp)
+        else:
+            sgd = Sgd(hp)
         return Softmax(sgd)
     raise NotImplementedError('Factory does not handle the constuction of the activation: '+act_string)
 
